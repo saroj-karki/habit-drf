e32ba247253956f6fe284f44ef8c28e171a648b1
Saroj Karki <karki.saroj28@gmail.com>
2020-12-21 12:30:26 +0545

    Adds habit_create, habit_update view

--- backend/urls.py
+++ backend/urls.py
@@ -7,4 +7,6 @@ from . import views
 urlpatterns =[
     path('habits/', views.habits),
     path('habit/<int:primary_key>/', views.habit_details),
+    path('habit-create/', views.habit_create),
+    path('habit-update/<int:primary_key>/', views.habit_update),
 ]
--- backend/views.py
+++ backend/views.py
@@ -24,3 +24,27 @@ def habit_details(request, primary_key):
         raise NotFound() from exception
     habit_obj = serializers.HabitSerializer(habit_obj)
     return Response(habit_obj.data)
+
+
+@api_view(['POST'])
+def habit_create(request):
+    '''Creates habit object'''
+    serializer = serializers.HabitSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(status=201)
+    return Response(serializer.errors, status=400)
+
+
+@api_view(['POST'])
+def habit_update(request, primary_key):
+    '''Updates habit object'''
+    try:
+        habit_obj = models.Habit.objects.get(id=primary_key)
+    except models.Habit.DoesNotExist as exception:
+        raise NotFound() from exception
+    serializer = serializers.HabitSerializer(instance=habit_obj, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(status=200)
+    return Response(serializer.errors, status=400)
